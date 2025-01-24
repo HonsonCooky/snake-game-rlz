3939e49cbf4cb704e0c3898b051560b52a702fdb
HarrisonCook <HonsonCooky@gmail.com>
2025-01-24 13:14:56 +1300

    restart with heap in mind

--- src/main.zig
+++ src/main.zig
@@ -2,21 +2,16 @@ const std = @import("std");
 const rl = @import("raylib");
 const rm = @import("raymath");
 
-const Snake = struct {
-    position: rl.Vector2,
-    _direction: rl.Vector2 = rl.Vector2.zero(),
+const SnakeNode = struct {
+    x: i32,
+    y: i32,
+    next: ?*SnakeNode,
+};
 
-    pub fn update(self: *Snake) void {
-        if (rl.isKeyDown(rl.KeyboardKey.w)) {
-            self._direction = rl.Vector2{ .x = 0, .y = -1 };
-        } else if (rl.isKeyDown(rl.KeyboardKey.s)) {
-            self._direction = rl.Vector2{ .x = 0, .y = 1 };
-        } else if (rl.isKeyDown(rl.KeyboardKey.a)) {
-            self._direction = rl.Vector2{ .x = -1, .y = 0 };
-        } else if (rl.isKeyDown(rl.KeyboardKey.d)) {
-            self._direction = rl.Vector2{ .x = 1, .y = 0 };
-        }
-    }
+const Player = struct {
+    head: *SnakeNode,
+
+    pub fn init() Player {}
 };
 
 pub fn main() void {
@@ -26,10 +21,21 @@ pub fn main() void {
     rl.initWindow(screenWidth, screenHeight, "Snake Game");
     defer rl.closeWindow();
 
+    rl.setTargetFPS(60);
+
+    var timeSinceMove: f32 = 0;
+    const movementTime = 0.2;
+
     // Main Game Loop
     while (!rl.windowShouldClose()) {
         // Update
-        {}
+        {
+            if (timeSinceMove < movementTime) {
+                timeSinceMove += rl.getFrameTime();
+            } else {
+                timeSinceMove = 0;
+            }
+        }
 
         // Draw
         {
